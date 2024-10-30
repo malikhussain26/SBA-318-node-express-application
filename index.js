77ca2d80fe1c1b82cbf79cf7c86a843b4bd258f7
malikhussain26 <hussain.am2697@gmail.com>
2024-10-29 21:18:37 -0500

    adding views

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const routes = require('./routes/routes');
+
 const app = express();
 const PORT = 3000;
 
-app.listen(PORT, () => {
+
+// Set different properties for the app
+app.set('view engine', 'ejs');
+app.set('views', './views')
+
+// Routes
+app.get('/', (req, res) => {
+  res.render('index.ejs', {
+    pageTitle: 'Welcome to Your To-Do List',
+    welcomeMessage: 'Add a New Task To Your To-Do List:',
+  });
+})
+  
+
+  // body-parser middle ware
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  // come back to this later
+  // app.use('/', routes);
+
+
+  // Listening for requests
+  app.listen(PORT, () => {
     console.log(`Server listening on port: ${PORT}.`);
   });
